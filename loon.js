1cb0caed162bef2e970618081067b1362fffeeac
1Rasy <78337335+1Rasy@users.noreply.github.com>
2025-08-24 01:14:40 +0800

    更新 loon.js

--- loon.js
+++ loon.js
@@ -1,22 +1,2 @@
-// 阻止 gw.xiaocantech.com 返回的剪贴板字段被复制
-let body = $response.body;
-
-try {
-  let json = JSON.parse(body);
-
-  if (json?.ul) {
-    const clipFields = ["mt_pwd", "ele_pwd", "jd_pwd", "pwd"];
-    clipFields.forEach(f => {
-      if (json.ul[f]) {
-        console.log(`已清空字段: ${f}`);
-        json.ul[f] = "";
-      }
-    });
-  }
-
-  body = JSON.stringify(json);
-} catch (e) {
-  console.log("解析 JSON 失败:", e);
-}
-
-$done({ body });
+if (req_url.includes("adapi.waimai.meituan.com/api/ad/landingPage")) {
+    console.log('美团 开始');
