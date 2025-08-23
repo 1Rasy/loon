6eeef1d0ac852048581ccc46efba8d3328d93a6d
1Rasy <78337335+1Rasy@users.noreply.github.com>
2025-08-24 02:01:25 +0800

    更新 loon.js

--- public/loon.js
+++ public/loon.js
@@ -1,3 +1,9 @@
+const $ = new API("获取Cookie或Token通用脚本");
+const req_url = $request.url;
+const req_headers = $request.headers;
+const req_body = $request.body;
+let rsp_body = "{}";
+
 if (req_url.includes("adapi.waimai.meituan.com/api/ad/landingPage")) {
     console.log('美团 开始');
     $.notify('美团token 获取成功✅', '', '');
