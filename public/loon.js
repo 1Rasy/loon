613efe8853900a8d197ef17442faa1005c8379a5
1Rasy <78337335+1Rasy@users.noreply.github.com>
2025-08-24 02:20:07 +0800

    更新 loon.js

--- public/loon.js
+++ public/loon.js
@@ -5,5 +5,5 @@ let rsp_body = "{}";
 
 if (req_url.includes("adapi.waimai.meituan.com/api/ad/landingPage")) {
     console.log('美团 开始');
-    $.notify('美团token 获取成功✅', '', '');
+    notify('美团token 获取成功✅', '', '');
 }
