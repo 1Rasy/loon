45bde3aa72f4482b6b9c781d62fa7f15211cbb1e
1Rasy <78337335+1Rasy@users.noreply.github.com>
2025-08-25 03:43:19 +0800

    更新 mt_cookie.js

--- public/mt_cookie.js
+++ public/mt_cookie.js
@@ -71,7 +71,7 @@
 
     const success = $persistentStore.write(JSON.stringify(storedArr), storeKey);
     if (success) {
-      $notification.post("美团Cookie",`✅ ${phone}获取成功`," ");
+      $notification.post("美团Cookie",`✅ ${phone}获取成功`,"");
       console.log(`[Loon] 已保存账号 ${phone} 的信息: ` + JSON.stringify(toStore));
     } else {
       console.log("[Loon] 保存本地失败");
