851d18a7800abcc7bb3598c066a4a6b551e2db73
1Rasy <78337335+1Rasy@users.noreply.github.com>
2025-08-25 12:03:58 +0800

    创建 test.js

---
+++ public/test.js
@@ -0,0 +1,14 @@
+/**
+ * 测试脚本
+ * 目标：当检测到 www.rasy.com 发出的请求，并且请求头包含 "123" 时，弹出通知成功
+ */
+
+const url = $request.url;
+const headers = $request.headers;
+
+// 判断目标网址 + 请求头
+if (url.includes("www.rasy.com") && headers && headers["123"]) {
+  $notification.post("测试脚本", "检测成功", "www.rasy.com 请求头包含 123");
+}
+
+$done({});
