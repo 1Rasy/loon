68a726a929a3c62a76d414474b486a30f54a77e0
1Rasy <78337335+1Rasy@users.noreply.github.com>
2025-08-25 12:09:01 +0800

    更新 looon.js

--- public/looon.js
+++ public/looon.js
@@ -10,11 +10,11 @@
 #!晓晓测试 = switch,false,tag=晓晓测试
 
 cron "17 7 * * *" script-path=https://raw.githubusercontent.com/fmz200/wool_scripts/main/Scripts/xxyx/xxyx_signin.js, timeout=300, tag=晓晓优选每日任务, img-url=https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/apps/xxyx_01.jpeg
-http-request ^https:\/\/xxyx-client-api\.xiaoxiaoyouxuan\.com\/my script-path=https://loon-nine.vercel.app/xxyx.js, requires-body=true, timeout=60, tag=晓晓测试, enable={晓晓测试}
+http-respnse ^https:\/\/xxyx-client-api\.xiaoxiaoyouxuan\.com\/my script-path=https://loon-nine.vercel.app/xxyx.js, requires-body=true, timeout=60, tag=晓晓测试, enable={晓晓测试}
 http-response ^https:\/\/adapi\.waimai\.meituan\.com\/api\/ad\/landingPage script-path=https://loon-nine.vercel.app/loon.js, requires-body=true, timeout=60, tag=提取津贴, enable={津贴提取}
 
 [Script]
-http-response ^https:\/\/www\.rasy\.com script-path=https://loon-nine.vercel.app/test.js, requires-body=true, timeout=10, tag=联动, enable={联动}
+http-request ^https:\/\/www\.rasy\.com script-path=https://loon-nine.vercel.app/test.js, requires-body=true, timeout=10, tag=联动, enable={联动}
 
 http-response ^https:\/\/offsiteact\.meituan\.com\/act\/ge\/queryPoiByRecallBiz script-path=https://loon-nine.vercel.app/美团.js, requires-body=true, timeout=60, tag=美团领券, enable={美团领券}
 
