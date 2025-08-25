e735d61691fe2d466d63ec53147aac54aa81e722
1Rasy <78337335+1Rasy@users.noreply.github.com>
2025-08-25 12:19:58 +0800

    更新 test.js

--- public/test.js
+++ public/test.js
@@ -1,13 +1,10 @@
-/**
- * 测试脚本
- * 目标：当检测到 www.rasy.com 发出的请求，并且请求头包含 "123" 时，弹出通知成功
- */
+
 
 const url = $request.url;
 const headers = $request.headers;
 
 // 判断目标网址 + 请求头
-if (url.includes("https://httpbin.org/get") && headers && headers["123"]) {
+if (url.includes("https://httpbin.org") && headers && headers["123"]) {
   $notification.post("测试脚本", "检测成功", "https://httpbin.org/get 请求头包含 123");
 }
 
