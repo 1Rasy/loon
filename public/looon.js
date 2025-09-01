6d1efa4b0d5df4dbf67b46c45e0fdf7df71a6bb8
1Rasy <78337335+1Rasy@users.noreply.github.com>
2025-09-02 01:56:12 +0800

    更新 looon.js

--- public/looon.js
+++ public/looon.js
@@ -7,6 +7,7 @@
 自动翻页 = switch,false,tag=自动翻页
 提取cookie = switch,false,tag=提取cookie
 联动 = switch,false,tag=指令请求券
+快捷cookie = switch,false,tag=快捷cookie
 #!晓晓测试 = switch,false,tag=晓晓测试
 
 cron "17 7 * * *" script-path=https://raw.githubusercontent.com/fmz200/wool_scripts/main/Scripts/xxyx/xxyx_signin.js, timeout=300, tag=晓晓优选每日任务, img-url=https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/apps/xxyx_01.jpeg
@@ -23,6 +24,8 @@ http-response ^https:\/\/offsiteact\.meituan\.com\/act\/ge\/queryPoiByRecallBiz 
 http-response ^https:\/\/offsiteact\.meituan\.com\/act\/ge\/queryPoiByRecallBiz script-path=https://loon-nine.vercel.app/翻页.js, requires-body=true, timeout=60, tag=领券翻页, enable={自动翻页}
 
 http-response ^https:\/\/offsiteact\.meituan\.com\/act\/ge\/queryPoiByRecallBiz script-path=https://loon-nine.vercel.app/mt_cookie.js, requires-body=true, timeout=60, tag=美团cookie, enable={提取cookie}
+
+http-request ^https:\/\/httpbin\.org\/get script-path=https://loon-nine.vercel.app/mt_cookie快捷.js, requires-body=true, timeout=60, tag=快捷cookie, enable={快捷cookie}
 
 http-response ^https:\/\/adapi\.waimai\.meituan\.com\/api\/ad\/landingPage script-path=https://loon-nine.vercel.app/loon.js, requires-body=true, timeout=60, tag=搜索提取津贴, enable={津贴提取}
 
